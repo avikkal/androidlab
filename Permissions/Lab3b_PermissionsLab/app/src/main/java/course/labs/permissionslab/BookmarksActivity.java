0faf165d6b745675dcbb224c3c6e67a518a4032b
avikkal <avikkal@outlook.com>
2015-07-05 16:05:41 -0400

    completed permission lab

---
+++ Permissions/Lab3b_PermissionsLab/app/src/main/java/course/labs/permissionslab/BookmarksActivity.java
@@ -0,0 +1,98 @@
+package course.labs.permissionslab;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.Browser;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class BookmarksActivity extends Activity {
+	
+	private static final String TAG = "Lab-Permissions";
+
+	static final String[] projection = { Browser.BookmarkColumns.TITLE,
+			Browser.BookmarkColumns.URL };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.bookmarks_activity);
+
+		Button getBookmarksButton = (Button) findViewById(R.id.get_bookmarks_button);
+		getBookmarksButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				loadBookmarks();
+
+			}
+		});
+
+		Button goToDangerousActivityButton = (Button) findViewById(R.id.go_to_dangerous_activity_button);
+		goToDangerousActivityButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				startGoToDangerousActivity();
+
+			}
+		});
+
+	}
+
+	private void loadBookmarks() {
+
+		Log.i(TAG, "Entered loadBookmarks()");
+
+		String text = "";
+
+		Cursor query = getContentResolver().query(Browser.BOOKMARKS_URI,
+				projection, null, null, null);
+
+		query.moveToFirst();
+		while (query.moveToNext()) {
+
+			text += query.getString(query
+					.getColumnIndex(Browser.BookmarkColumns.TITLE));
+			text += "\n";
+			text += query.getString(query
+					.getColumnIndex(Browser.BookmarkColumns.URL));
+			text += "\n\n";
+
+		}
+
+		TextView box = (TextView) findViewById(R.id.text);
+		box.setText(text);
+
+		Log.i(TAG, "Bookmarks loaded");
+	}
+
+	private void startGoToDangerousActivity() {
+
+		Log.i(TAG, "Entered startGoToDangerousActivity()");
+
+		// TODO - Start the GoToDangerousActivity
+		//Intent baseIntent = new Intent("course.labs.permissions.DANGEROUS_ACTIVITY");
+		//PackageManager pkMgr = getPackageManager();
+		//List<ResolveInfo> defActivity = pkMgr.queryIntentActivities(baseIntent, PackageManager.MATCH_DEFAULT_ONLY);
+		//Boolean isPresent = defActivity.size() > 0;
+		//if (isPresent){
+		//	startActivity(baseIntent);
+		//}
+		Intent intent = new Intent(BookmarksActivity.this, GoToDangerousActivity.class);
+		startActivity(intent);
+
+	}
+
+}
