2f8626e3c5abdfd096dccf3b9a6693aed89c30d5
avikkal <avikkal@outlook.com>
2015-08-22 21:15:16 -0400

    daily selfie

--- dailyselfie/app/src/main/java/course/example/dailyselfie/MainActivity.java
+++ dailyselfie/app/src/main/java/course/example/dailyselfie/MainActivity.java
@@ -1,17 +1,80 @@
 package course.example.dailyselfie;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+//import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
 
+public class MainActivity extends ListActivity {
+    private ListView mListView;
+    private ImageListAdapter mAdapter;
+    private File savedImagePath;
+    private GestureDetector mGestureDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        mListView = getListView();
+        mAdapter = new ImageListAdapter(this);
+        setListAdapter(mAdapter);
+        SelfieAlarm alarm =new SelfieAlarm(this);
+        alarm.setAlarm();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mAdapter.getCount() == 0) {
+            for(File f:PreviewManager.loadSelfies()){
+                mAdapter.add(new SelfiePlaceHolder(f.getAbsolutePath()));
+           }
+        }
+        setupGestureDetector();
+        View.OnTouchListener gestureListener = new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                return mGestureDetector.onTouchEvent(event);
+            }};
+        mListView.setOnTouchListener(gestureListener);
+
+   }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    private void setupGestureDetector(){
+        mGestureDetector = new GestureDetector(this,
+                                        new PreviewManager(this,mListView, mAdapter));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        // TODO - Delegate the touch to the gestureDetector
+        if (mGestureDetector != null) {
+            mGestureDetector.onTouchEvent(event);
+        }
+        return false;
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,9 +92,75 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            return takeASelfie();
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    public boolean takeASelfie(){
+        if (!isCameraPresent()){
+            Toast.makeText(this, "No camera. Please enable camera on your emulator",
+                        Toast.LENGTH_LONG).show();
+        }
+        else {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            Uri fileUri = null;
+            try {
+                savedImagePath = SelfiePlaceHolder.getOutputMediaFile(SelfiePlaceHolder
+                        .MEDIA_TYPE_IMAGE, SelfiePlaceHolder.SelfieFileType.Permanent);
+            }catch (IOException e) {
+               Toast.makeText(this, "No external storage. Add an external storage ( SD card) on " +
+                                    "your emulator",
+                            Toast.LENGTH_LONG).show();
+            }
+            fileUri = Uri.fromFile(savedImagePath);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); //
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+
+        }
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // Image captured and saved to fileUri specified in the Intent
+                Log.d("DailySelfieApp", "Image saved to: " +
+                        savedImagePath.getAbsolutePath());
+                mAdapter.add(new SelfiePlaceHolder(savedImagePath.getAbsolutePath()));
+                mListView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Select the last row so it will scroll into view...
+                        mListView.setSelection(mAdapter.getCount() - 1);
+                    }
+                });
+
+            } else if (resultCode == RESULT_CANCELED) {
+                // User cancelled the image capture
+            } else {
+                Toast.makeText(this, "Image capture failed ", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private boolean isCameraPresent(){
+        boolean isPresent = this.getPackageManager().hasSystemFeature(PackageManager
+                .FEATURE_CAMERA_ANY);
+        if (isPresent){
+            Camera c = null;
+            try {
+                c = Camera.open(); // attempt to get a Camera instance
+            }
+            catch (Exception e){
+                // Camera is not available (in use or does not exist)
+                isPresent = false;
+            }
+        }
+        return isPresent;
     }
 }
--- dailyselfie/app/src/main/java/course/example/dailyselfie/PreviewManager.java
+++ dailyselfie/app/src/main/java/course/example/dailyselfie/PreviewManager.java
@@ -1,43 +1,106 @@
 package course.example.dailyselfie;
 
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class PreviewManager extends GestureDetector.SimpleOnGestureListener {
     private ListView mPreviewList;
+    private ImageListAdapter mListAdapter;
+    private Context mContext;
+    private static final int MIN_SWIPE_DISTANCE_FOR_REMOVE = 150;
+    private static final int MIN_SWIPE_VELOCITY_FOR_REMOVE = 200;
 
-    public PreviewManager(ListView list) {
+    public PreviewManager(Context context, ListView list, ImageListAdapter listAdapter) {
         mPreviewList = list;
+        mListAdapter = listAdapter;
+        mContext = context;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        super.onSingleTapConfirmed(e);
+        int pos = mPreviewList.pointToPosition((int)e.getX(), (int)e.getY());
+        SelfiePlaceHolder s = (SelfiePlaceHolder)mListAdapter.getItem(pos);
+        if (s != null) {
+            Intent imageIntent = new Intent(mContext, ImageActivity.class);
+            imageIntent.putExtra("selfie_path", s.getFullImagePath());
+            mContext.startActivity(imageIntent);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        super.onDoubleTap(e);
+        int pos = mPreviewList.pointToPosition((int)e.getX(), (int)e.getY());
+        SelfiePlaceHolder s = (SelfiePlaceHolder)mListAdapter.getItem(pos);
+        if (s != null) {
+            Intent imageIntent = new Intent(mContext, ImageActivity.class);
+            imageIntent.putExtra("selfie_path", s.getFullImagePath());
+            mContext.startActivity(imageIntent);
+            return true;
+        }
+        return false;
     }
 
     //CONDITIONS ARE TYPICALLY VELOCITY OR DISTANCE
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
         //if (INSERT_CONDITIONS_HERE)
-            if (showDeleteButton(e1))
+        if(e2.getX() - e1.getX() > MIN_SWIPE_DISTANCE_FOR_REMOVE && Math.abs(velocityX) >
+                MIN_SWIPE_VELOCITY_FOR_REMOVE) {
+            if (deleteImage(e1)) {
                 return true;
-        return super.onFling(e1, e2, velocityX, velocityY);
-    }
-
-    private boolean showDeleteButton(MotionEvent e1) {
-        int pos = mPreviewList.pointToPosition((int)e1.getX(), (int)e1.getY());
-        return showDeleteButton(pos);
-    }
-
-    private boolean showDeleteButton(int pos) {
-        View child = mPreviewList.getChildAt(pos);
-        if (child != null){
-            //Button delete = (Button) child.findViewById(R.id.delete_button_id);
-            // if (delete != null)
-            //    if (delete.getVisibility() == View.INVISIBLE)
-            //        delete.setVisibility(View.VISIBLE);
-            //   else
-            //        delete.setVisibility(View.INVISIBLE);
-            return true;
+            }
+        }
+        else
+        {
+            Log.d("DailySlfieApp", "Not a flig from Left to Right");
         }
         return false;
+
+        //return super.onFling(e1, e2, velocityX, velocityY);
     }
+
+    private boolean deleteImage(MotionEvent e1) {
+        int pos = mPreviewList.pointToPosition((int)e1.getX(), (int)e1.getY());
+        mListAdapter.remove(pos);
+        return false;
+    }
+
+    private void  showImageInfullScreen(int itemPosition){
+        SelfiePlaceHolder item = (SelfiePlaceHolder)    mListAdapter.getItem(itemPosition);
+        if (item != null){
+            String imagePath = item.getFullImagePath();
+        }
+    }
+
+    public static ArrayList<File>loadSelfies(){
+
+        ArrayList<File> selfies = new ArrayList<>();
+        try {
+            File selfesRoot = SelfiePlaceHolder.getRootMediaDir();
+            for (File f : selfesRoot.listFiles()){
+                if (f.length() > 0 ){
+                    selfies.add(f);
+                }
+            }
+
+        }catch (IOException e){
+
+        }
+        return selfies;
+    }
+
 }
